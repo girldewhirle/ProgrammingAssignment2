df66e44f2488d39003c2c6dad6efce25e0559f2a
girldewhirle <rebecca.ratcliffe@gmail.com>
2015-02-18 23:41:07 +0000

    Revert "Revert "Changes t""
    This reverts commit 7ce71bfde9e9e8d491921ee0d6e5243d536cdab7.

--- cachematrix.R
+++ cachematrix.R
@@ -11,7 +11,7 @@ makeCacheMatrix <- function(x = matrix()) {
     m <<- NULL
   }
   get <- function() x
-  setsolve <- function(mean) m <<- solve
+  setsolve <- function(solve) m <<- solve
   getsolve <- function() m
   list(set = set, get = get,
        setsolve = setsolve,
@@ -26,4 +26,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
